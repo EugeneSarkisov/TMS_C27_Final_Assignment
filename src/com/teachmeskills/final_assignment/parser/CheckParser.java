05f03096f1563bcc687a8a72cfc76dbefed4151f
EugeneSarkisov <eugenesarkisov@gmail.com>
2024-03-06 19:39:24 +0300

    TMS.FinalAssignment: modified prototype of CheckParser.java;
    0.1 - prototype get all necessary info from data/checks and sum it in variable checkSum. Also, handled with exceptions (isEmptyValidation). Need to implement second exception isDirectoryValidation.
    0.2 - remove all validation for time. CheckParser.java now move all garbage checks into temp/garbageChecks. Also, encapsulated all logic in private methods and made regex validation of files.

--- src/com/teachmeskills/final_assignment/parser/CheckParser.java
+++ src/com/teachmeskills/final_assignment/parser/CheckParser.java
@@ -1,56 +1,67 @@
 package com.teachmeskills.final_assignment.parser;
 
-import com.teachmeskills.final_assignment.custom_exceptions.EmptyFolderException;
-import com.teachmeskills.final_assignment.validation.DirectoryValidation;
 
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
 
+
 public class CheckParser {
-    public static void parseCheckInfo(){
+    public static void parseCheckInfo() {
+        //TODO validationMethod
+        sortChecks();
+        parseInfo();
+    }
+
+    private static List<File> sortChecks() {
+        //remove all unnecessary files
         File checkFolder = new File("data/checks");
-        try {
-            //making validations on folder
-            //DirectoryValidation.isDirectoryValidation(checkFolder);
-            DirectoryValidation.isEmptyValidation(checkFolder);
-            //remove all unnecessary files
-            List<File> checks = new ArrayList<>(Arrays.asList(checkFolder.listFiles()));
-            Iterator<File> checkIter = checks.iterator();
-            while (checkIter.hasNext()){
-                File checkList = checkIter.next();
-                if (!checkList.getName().endsWith(".txt") || !checkList.getName().startsWith("2023")){
-                    checkIter.remove();
+        List<File> checks = new ArrayList<>(List.of(checkFolder.listFiles()));
+        Iterator<File> checkIter = checks.iterator();
+        while (checkIter.hasNext()) {
+            File check = checkIter.next();
+            if (!check.getName().matches("^2023_Electric_Bill_[0-9]{2,}[.]{1}txt$")) {
+                try {
+                    Files.move(Paths.get(check.getPath()),
+                            Paths.get("data/temp/garbageChecks/" + check.getName()),
+                            StandardCopyOption.REPLACE_EXISTING);
+                } catch (IOException e) {
+                    System.out.println("Error while moving file: " + e.getMessage());
+                } catch (Exception e) {
+                    System.out.println("Something went wrong... Please, contact with the administrator.");
                 }
+                checkIter.remove();
             }
-            //read all necessary checks and get strings with bill sum
-            List<String> billList = new ArrayList<>();
-            for (File files : checks){
-                BufferedReader checkReader = new BufferedReader(new FileReader(files));
+        }
+        return checks;
+    }
+
+    private static void parseInfo() {
+        //parsing check info
+        List<String> billList = new ArrayList<>();
+        for (File check : sortChecks()) {
+            try (BufferedReader checkReader = new BufferedReader(new FileReader(check))) {
                 String line;
-                while ((line = checkReader.readLine()) != null){
-                    if (line.startsWith("Bill")){
+                while ((line = checkReader.readLine()) != null) {
+                    if (line.startsWith("Bill")) {
                         billList.add(line);
                     }
                 }
-                checkReader.close();
+            } catch (Exception e) {
+                e.printStackTrace();
             }
-            //summing the bill sums
-            double checkSum = 0.0;
-            for (String bill : billList) {
-                checkSum += Double.parseDouble(bill.substring(23).trim().replace(",", "."));
-                System.out.println(checkSum);
-            }
-        } catch (EmptyFolderException e) {
-            System.err.println(e.getMessage());
-        } catch (Exception e){
-            e.printStackTrace();
+        }
+        //summing all necessary bills
+        double checkSum = 0.0;
+        for (String bill : billList) {
+            //TODO currencyConvertor
+            checkSum += Double.parseDouble(bill.substring(23).trim().replace(",", "."));
+            System.out.println(checkSum);
         }
     }
-
-    public static void main(String[] args) {
-        parseCheckInfo();
-    }
 }
+
--- src/com/teachmeskills/final_assignment/run/Runner.java
+++ src/com/teachmeskills/final_assignment/run/Runner.java
@@ -1,7 +1,9 @@
 package com.teachmeskills.final_assignment.run;
 
+import com.teachmeskills.final_assignment.parser.CheckParser;
+
 public class Runner {
     public static void main(String[] args) {
-
+        CheckParser.parseCheckInfo();
     }
 }
